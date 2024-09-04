d4137de7baa5b6192b91bdeceb8cfbfe32b2fb49
LiuvaSalas <liuvasalas01@gmail.com>
2024-09-04 18:38:27 -0400

    actualizacion de docstrings en archivos foto y prueba.

--- prueba.py
+++ prueba.py
@@ -1,17 +1,22 @@
 from error import DimensionError
 from foto import Foto
 
-consultar = True
-while consultar:
-    try:
-        ancho = int(input("Ingresa el ancho de la foto:\n>"))
-        alto = int(input("Ingresa el alto de la foto:\n>"))
-        ruta = input("Ingresa la ruta:\n>")
-        prueba_foto = Foto(ancho, alto, ruta)
-        consultar = False
-    except DimensionError as e:
-        print(f"ERROR: {e}")
+def main():
+    """
+    Esta es la funcion principal que ejecuta las clases y metodos anidados entre si.
+    """
+    consultar = True
+    while consultar:
+        try:
+            ancho = int(input("Ingresa el ancho de la foto:\n>"))
+            alto = int(input("Ingresa el alto de la foto:\n>"))
+            ruta = input("Ingresa la ruta:\n>")
+            prueba_foto = Foto(ancho, alto, ruta)
+            consultar = False
+        except DimensionError as e:
+            print(f"ERROR: {e}")
 
-print("Valores de la foto valida!")
+    print("Valores de la foto valida!")
 
-#Aun me falta documentar y Readme
+if __name__ == "__main__":
+    main()
